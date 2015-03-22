6d2c3a3c654f4015c7291b063228629db059c642
Logan Lindquist <logan@legacybeta.com>
2015-03-22 00:29:45 -0500

    Adding bower and grunt to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,27 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
-        uglify: {
 
-        },
+            copy: {
+                main: {
+                    files: [
+
+                        // includes files within path and its sub-directories
+                        {expand: true, flatten: true, src: ['bower_components/jquery/dist/*'], dest: 'js/vendor/'},
+                        {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/js/*'], dest: 'js/vendor/'},
+                        {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/css/*'], dest: 'css/vendor/'},
+                        {expand: true, flatten: true, src: ['bower_components/fitvids/jquery.fitvids.js'], dest: 'js/vendor/'}
+
+                    ]
+                }
+            }
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['copy']);
 
 };
