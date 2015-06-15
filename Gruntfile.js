8b08c5d4dcf1df3c557399235224650bf7fd83a1
Logan Lindquist <logan@legacybeta.com>
2015-06-15 15:00:22 -0500

    updating grunt file; adding less task and changing formatting a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,45 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-            copy: {
-                main: {
-                    files: [
+        copy: {
+            main: {
+                files: [
 
-                        // includes files within path and its sub-directories
-                        {expand: true, flatten: true, src: ['bower_components/jquery/dist/*'], dest: 'js/vendor/'},
-                        {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/js/*'], dest: 'js/vendor/'},
-                        {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/css/*'], dest: 'css/vendor/'},
-                        {expand: true, flatten: true, src: ['bower_components/fitvids/jquery.fitvids.js'], dest: 'js/vendor/'}
+                    // includes files within path and its sub-directories
+                    {expand: true, flatten: true, src: ['bower_components/jquery/dist/*'], dest: 'js/vendor/'},
+                    {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/js/*'], dest: 'js/vendor/'},
+                    {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/css/*'], dest: 'css/vendor/'},
+                    {expand: true, flatten: true, src: ['bower_components/fitvids/jquery.fitvids.js'], dest: 'js/vendor/'}
 
-                    ]
+                ]
+            }
+        },
+
+        less: {
+            development: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "css/main.css": "css/main.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "css/main.css": "css/main.less"
                 }
             }
+        }
 
     });
 
     //grunt.loadNpmTasks('grunt-contrib-uglify');
     //grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('default', ['copy']);
 
