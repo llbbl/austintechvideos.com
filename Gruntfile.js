43bf0efd5f3c4017ef588e01fb7b93da3a84432c
Logan Lindquist <logan@legacybeta.com>
2015-06-19 15:50:59 -0500

    removing the uglify dependency that we don't need

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,8 +31,7 @@ module.exports = function(grunt) {
 
     });
 
-    //grunt.loadNpmTasks('grunt-contrib-uglify');
-    //grunt.loadNpmTasks('grunt-contrib-jshint');
+
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
 
